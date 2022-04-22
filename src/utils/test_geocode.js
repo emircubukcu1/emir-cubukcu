b59fe1136417e79d8864f18750719d112ebd921c
unknown <emir-200101@hotmail.com>
2022-04-22 09:44:07 +0300

    ikinci

--- src/utils/test_geocode.js
+++ src/utils/test_geocode.js
@@ -1,1 +1,2 @@
 const api="https://api.mapbox.com/geocoding/v5/mapbox.places/"+encodeURIComponent(address)+".json?acces_token=pk.eyJ1IjoiY3VidWtjdWVtaXIiLCJhIjoiY2wxZXFqeDd5MGR0NDNkcnRsNTQzOWFkayJ9.t2fh6Pn9OClsJpkcDR83pA";
+
